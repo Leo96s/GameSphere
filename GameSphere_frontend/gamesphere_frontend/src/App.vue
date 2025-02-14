7f445a8bb1027ce8067d9def4f6851faa695fae8
Leonardo Silva <leonardosilva.00009@gmail.com>
2025-02-14 18:40:24 +0000

    feat: Iniciado frontend em vue.js

---
+++ GameSphere_frontend/gamesphere_frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view name="header"></router-view>
+    <main>
+      <transition name="fade" mode="out-in">
+        <router-view/>
+      </transition>
+    </main>
+    <router-view name="footer"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
+
+
